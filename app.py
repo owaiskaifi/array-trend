e89eca57f49f14d2984a78e91cd68a1fcc6995ef
Muhammad Owais <43828642+owaiskaifi@users.noreply.github.com>
2022-06-03 17:10:25 +0500

    :)

--- app.py
+++ app.py
@@ -1,21 +1,52 @@
  
-from flask import Flask ,request
-import pickle
-import pandas as pd
+ 
+
+from flask import Flask ,request,render_template 
+from joblib import load
 import numpy as np
+
+
 app=Flask(__name__)
+clf =   load( './model.pkl'  )
+ 
+def get_status(y_pred_cycle ):    
+   
+    ct_status= ['NO_COUNT','INCREASE','DECREASE','STABLE','VARIANCE'] #  ['0','1','2','3','4']      
+    
+    ctt=ct_status[y_pred_cycle]
+    
+    return  ctt
+  
+@app.route('/')
+def home():
+    return render_template( 'index.html')
+ 
+@app.route('/predict', methods=['POST'])
+def predict():
 
-@app.route('/',methods=['POST'])
+    try:
 
+    
+        data=request.get_json(force=True)   
+        val_arr_ct=  np.asarray(data['arr']) 
 
-def perdict():
-    clf = pickle.load(open('./model.pkl', 'rb'))
-    data=request.get_json(force=True)
-    data=pd.DataFrame(data)
-    out=clf.predict(np.asarray(data['ct']).reshape(1, -1))
-    result={'Trend':int(out[0])}
-    return result 
+        nor=max(val_arr_ct)
+        
+        if nor!=0:
+            val_arr_ct= val_arr_ct / nor*2 
+            
+            out=clf.predict( val_arr_ct.reshape( 1,-1))  
+            
+        else:
+            out=np.array([0])
 
+          
+        out= get_status(int(out))
+        result={'STATUS': out  }
+        return result 
+    except:
+        result={"ERROR":'Wrong input format'}
+        return     result
 
 if __name__== '__main__':
-    app.run(port=5000,debug=False)
+    app.run(  )
